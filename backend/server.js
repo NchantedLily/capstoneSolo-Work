ea5bf48ea5a2b154c6b1dce4821da6289e18f906
Martia <pureromancebymartia@gmail.com>
2022-06-06 16:14:51 -0400

    welcome

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+        
+
+
+//connect to mogodb
+const dbURI = 'mongodb+srv://Martia:nchantedlily0785@capstone.2gv0g.mongodb.net/capstoneretryWrites=true&w=majority';
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log('connected to mongoDB');})
+
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.listen(port, () =>{
+    console.log(`Server is running on port ${port}`);
+});
